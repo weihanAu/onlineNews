bc77a271415e7003239a1072f7ae4e1ceb48f335
weihan <weihan@weihans-MBP.modem>
2022-03-28 10:08:17 +1100

    deploy my app

---
+++ precache-manifest.51b8e65079b6099c04c4abd6ad4a56ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b8578c65bbde4df84d4",
+    "url": "/static/css/main.94fb725b.chunk.css"
+  },
+  {
+    "revision": "7b8578c65bbde4df84d4",
+    "url": "/static/js/main.7b8578c6.chunk.js"
+  },
+  {
+    "revision": "ad5119cb3930767390ab",
+    "url": "/static/js/1.ad5119cb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6883e9c5128c51b390f1e6fc7dcdf6df",
+    "url": "/index.html"
+  }
+];
